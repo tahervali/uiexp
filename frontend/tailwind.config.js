601de14bb2a97ed9708b8930db2ce74a3016d0cd
root <root@ip-10-0-171-85.eu-west-1.compute.internal>
2025-06-18 15:03:12 +0000

    [HumBot/cdgn_01JY1RWTCXT9MXVX2QPRE5VF3Q] Update by HumBot CodeGen cdgn_01JY1RWTCXT9MXVX2QPRE5VF3Q

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        greenTheme: {
+          light: '#a3d9a5',
+          DEFAULT: '#4caf50',
+          dark: '#087f23',
+        },
+      },
+    },
   },
   plugins: [],
 }
